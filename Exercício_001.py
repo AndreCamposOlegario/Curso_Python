2c8d4761d2f16f9001f5fba74414f766369d5a50
André De Campos <andre.olegario.campos@gmail.com>
2022-05-27 08:56:11 -0300

    Add files via upload

---
+++ Exercício_001.py
@@ -0,0 +1,19 @@
+
+
+
+
+n0 = int(input('Digite um número: '))
+n1 = int(input('Digite um número: '))
+
+
+s = n0 + n1
+print('O resultado de {} + {} é: {}' .format(n0, n1, s))
+
+s = n0 - n1
+print('O resultado de {} - {} é: {}' .format(n0, n1, s))
+
+s = n0 * n1
+print('O resultado de {} * {} é: {}' .format(n0, n1, s))
+
+s = n0 / n1
+print('O resultado de {} / {} é: {}' .format(n0, n1, s))
