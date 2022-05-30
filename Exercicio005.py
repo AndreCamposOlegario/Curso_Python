16829056dfd8218e2053feded4b3572cad2fbd51
André De Campos <andre.olegario.campos@gmail.com>
2022-05-30 09:11:24 -0300

    Add files via upload

---
+++ Exercicio005.py
@@ -0,0 +1,3 @@
+vr = float(input('Digite o valor a ser convertido: '))
+cd = vr * 3.72
+print('${} Reais é igual a ${:.4} dolar canadence' .format(vr, cd))
---
+++ Exercicio006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um numero: '))
+
+d = n * 2
+t = n * 3
+rq = n ** (1/2)
+
+print(' O dobro de {} é {}\n O triplo de {} é {}\n A raiz quadrada de {} é {:.3} ' .format(n, d, n, t, n, rq))
